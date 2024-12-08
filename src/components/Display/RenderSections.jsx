d40f84e466302b2f8aed4720563b8dd3ff74a749
davidlara <dlara7913@gmail.com>
2024-12-07 17:54:56 -0800

    Created component to render multiple sections

--- src/components/Display/RenderSections.jsx
+++ src/components/Display/RenderSections.jsx
@@ -2,7 +2,7 @@ export default function RenderSection({array, Component , title }){
     return(
         <>
         <h3>{title}</h3>
-        {array.map(component => <Component props={component} key={component.id}/> )}
+        {array.map(component => <Component prop ={component} key={component.id}/> )}
         </>
     );
 }
